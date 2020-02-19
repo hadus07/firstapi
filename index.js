77a0a6fea46aa349c0154ad7ec6295c273d265b4
Hafidiy <hafid90@list.ru>
2020-02-19 21:44:19 +0500

    ss

--- index.js
+++ index.js
@@ -35,5 +35,15 @@ mongoose
 app.use('/api/people', people)
 app.use('/api/todos', todos)
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 4004
 app.listen(PORT, () => console.log(`server started on ${PORT}...`))
